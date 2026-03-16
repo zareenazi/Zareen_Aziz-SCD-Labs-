3dcc023ed5a3c971977fada74ea85c086748fff2
zareen aziz <zareenaziz61@gmail.com>
2026-03-16 09:37:43 +0500

    Added two files

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        Students s1 = new Students();
+        s1.name ="Zareen Aziz";
+        s1.rollNumber=5044;
+        s1.displayInfo();
+
+        Students s2= new Students();
+        s2.name = "Zarii";
+        s2.rollNumber= 5041;
+        s2.displayInfo();
+
+    }
+}
---
+++ Students.java
@@ -0,0 +1,13 @@
+public class Students {
+    String name;
+    int rollNumber;
+
+    public void displayInfo(){
+        System.out.println("Student Name is :" + name);
+        System.out.println("Student Roll No. is :" +rollNumber );
+
+        System.out.println("----------------------------");
+    }
+    
+}
+
